e117aafb605525d8d2c2f1c5f856b7e177bf01c3
Mario Adisurya <mario.adisurya@gmail.com>
2023-03-06 12:06:58 +1300

    feat: `js` -> `ts`

--- lib/remark-toc-headings.ts
+++ lib/remark-toc-headings.ts
@@ -2,9 +2,19 @@ import { visit } from 'unist-util-visit'
 import { slug } from 'github-slugger'
 import { toString } from 'mdast-util-to-string'
 
-export default function remarkTocHeadings(options) {
+interface ExportRef {
+  value: string
+  url: string
+  depth: number
+}
+
+interface Options {
+  exportRef: ExportRef[]
+}
+
+export default function remarkTocHeadings(options: Options) {
   return (tree) =>
-    visit(tree, 'heading', (node, index, parent) => {
+    visit(tree, 'heading', (node) => {
       const textContent = toString(node)
       options.exportRef.push({
         value: textContent,
--- lib/tags.ts
+++ lib/tags.ts
@@ -6,8 +6,8 @@ import kebabCase from './utils/kebabCase'
 
 const root = process.cwd()
 
-export async function getAllTags(type) {
-  const files = await getFiles(type)
+export function getAllTags(type: string) {
+  const files = getFiles(type)
 
   let tagCount = {}
   // Iterate through each post, putting all found tags into `tags`
@@ -15,7 +15,7 @@ export async function getAllTags(type) {
     const source = fs.readFileSync(path.join(root, 'data', type, file), 'utf8')
     const { data } = matter(source)
     if (data.tags && data.draft !== true) {
-      data.tags.forEach((tag) => {
+      data.tags.forEach((tag: string) => {
         const formattedTag = kebabCase(tag)
         if (formattedTag in tagCount) {
           tagCount[formattedTag] += 1
