875b844ebf144cdbe685a3bcd3502cd9cde63aed
Mario Adisurya <mario.adisurya@gmail.com>
2023-03-07 17:07:22 +1300

    refactor: guard clause

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -31,28 +31,30 @@ const ThemeSwitch = () => {
   // When mounted on client, now we can show the UI
   useEffect(() => setMounted(true), [])
 
+  if (!mounted) {
+    return null
+  }
+
   return (
-    mounted && (
-      <label
-        aria-label="Toggle Dark Mode"
-        className="relative inline-flex items-center cursor-pointer"
-      >
-        <input
-          type="checkbox"
-          value=""
-          className="sr-only peer"
-          onClick={() => setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark')}
-        />
-        <div className="w-14 h-7 flex items-center justify-evenly bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600">
-          <SvgWrapper>
-            <LightModeIcon />
-          </SvgWrapper>
-          <SvgWrapper>
-            <DarkModeIcon />
-          </SvgWrapper>
-        </div>
-      </label>
-    )
+    <label
+      aria-label="Toggle Dark Mode"
+      className="relative inline-flex items-center cursor-pointer"
+    >
+      <input
+        type="checkbox"
+        value=""
+        className="sr-only peer"
+        onClick={() => setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark')}
+      />
+      <div className="w-14 h-7 flex items-center justify-evenly bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600">
+        <SvgWrapper>
+          <LightModeIcon />
+        </SvgWrapper>
+        <SvgWrapper>
+          <DarkModeIcon />
+        </SvgWrapper>
+      </div>
+    </label>
   )
 }
 
