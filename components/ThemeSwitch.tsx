a932c2d41a3f655ed1812b082d118b485e89a1f4
Mario Adisurya <mario.adisurya@gmail.com>
2023-03-10 16:08:05 +1300

    chore: `checked` -> `defaultChecked`

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -46,7 +46,7 @@ const ThemeSwitch = () => {
         type="checkbox"
         value=""
         className="sr-only peer"
-        checked={isDarkMode}
+        defaultChecked={isDarkMode}
         onClick={() => setTheme(isDarkMode ? 'light' : 'dark')}
       />
       <div className="w-14 h-7 flex items-center justify-evenly bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600">
